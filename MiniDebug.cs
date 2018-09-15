3210aec2395c272a7a94588c278824d91818c058
Alex Marmura <krythomia@gmail.com>
2018-09-15 17:53:07 -0300

    Crash fix, more savestate functions.

--- MiniDebug.cs
+++ MiniDebug.cs
@@ -13,13 +13,9 @@ public class ModCheats : MonoBehaviour
 	{
 	}
 
-	// Token: 0x06003043 RID: 12355
+	// Token: 0x06003043 RID: 12355 RVA: 0x00121EA8 File Offset: 0x001200A8
 	public void Update()
 	{
-		if (!HeroController.instance.acceptingInput)
-		{
-			return;
-		}
 		if (Input.GetKeyDown(KeyCode.W))
 		{
 			HeroController.instance.StartCoroutine(HeroController.instance.Die());
@@ -84,42 +80,49 @@ public class ModCheats : MonoBehaviour
 			GameManager.instance.cameraCtrl.camTarget.transform.position = new Vector3(HeroController.instance.transform.position.x, HeroController.instance.transform.position.y, GameManager.instance.cameraCtrl.camTarget.transform.position.z);
 			GameManager.instance.cameraCtrl.transform.position = new Vector3(HeroController.instance.transform.position.x, HeroController.instance.transform.position.y, GameManager.instance.cameraCtrl.transform.position.z);
 		}
-		PlayerData instance = PlayerData.instance;
-		HeroController instance2 = HeroController.instance;
-		GameManager instance3 = GameManager.instance;
+		this.pd = PlayerData.instance;
+		this.hc = HeroController.instance;
+		this.gm = GameManager.instance;
 		if (Input.GetKeyDown(KeyCode.O))
 		{
-			this.SavePos.y = instance2.gameObject.transform.position.y;
-			this.SavePos.x = instance2.gameObject.transform.position.x;
-			this.SaveScene = instance3.GetSceneNameString();
-			this.SaveDeathstate = instance.soulLimited;
-			this.SaveShadePos.x = instance.shadePositionX;
-			this.SaveShadePos.y = instance.shadePositionY;
-			this.SaveShadeScene = instance.shadeScene;
-			this.SaveHealth = instance.health;
-			this.SaveGeo = instance.geo;
+			this.SavePos.y = this.hc.gameObject.transform.position.y;
+			this.SavePos.x = this.hc.gameObject.transform.position.x;
+			this.SaveScene = this.gm.GetSceneNameString();
+			this.SaveDeathstate = this.pd.soulLimited;
+			this.SaveShadePos.x = this.pd.shadePositionX;
+			this.SaveShadePos.y = this.pd.shadePositionY;
+			this.SaveShadeScene = this.pd.shadeScene;
+			this.SaveHealth = this.pd.health;
+			this.SaveGeo = this.pd.geo;
+			this.SaveMaxMP = this.pd.maxMP;
+			this.SaveShadeMP = this.pd.shadeMP;
+			this.SaveShadeGeo = this.pd.geoPool;
 			this.SaveMade = true;
 		}
 		if (Input.GetKeyDown(KeyCode.L) && this.SaveMade)
 		{
-			instance3.ChangeToScene(this.SaveScene, "", 0f);
+			this.hc.gameObject.transform.position = this.SavePos;
+			this.gm.ChangeToScene(this.SaveScene, "", 0f);
 			this.cameraFollow = true;
-			instance3.ResetSemiPersistentItems();
-			instance.soulLimited = this.SaveDeathstate;
-			instance.geo = this.SaveGeo;
-			while (instance.health < this.SaveHealth)
+			this.gm.ResetSemiPersistentItems();
+			this.pd.soulLimited = this.SaveDeathstate;
+			this.pd.geo = this.SaveGeo;
+			this.pd.maxMP = this.SaveMaxMP;
+			this.pd.shadeMP = this.SaveShadeMP;
+			this.pd.geoPool = this.SaveShadeGeo;
+			while (this.pd.health > this.SaveHealth)
 			{
-				instance2.TakeHealth(1);
+				this.hc.TakeHealth(1);
 			}
-			while (instance.health < this.SaveHealth)
+			while (this.pd.health < this.SaveHealth)
 			{
-				instance2.AddHealth(1);
+				this.hc.AddHealth(1);
 			}
 			if (this.SaveDeathstate)
 			{
-				instance.shadePositionX = this.SaveShadePos.x;
-				instance.shadePositionY = this.SaveShadePos.y;
-				instance.shadeScene = this.SaveShadeScene;
+				this.pd.shadePositionX = this.SaveShadePos.x;
+				this.pd.shadePositionY = this.SaveShadePos.y;
+				this.pd.shadeScene = this.SaveShadeScene;
 			}
 		}
 	}
@@ -175,7 +178,7 @@ public class ModCheats : MonoBehaviour
 		base.StartCoroutine(this.DestroyBordersCoroutine());
 	}
 
-	// Token: 0x06003049 RID: 12361 RVA: 0x001223F0 File Offset: 0x001205F0
+	// Token: 0x06003049 RID: 12361 RVA: 0x001224BC File Offset: 0x001206BC
 	public void OnGUI()
 	{
 		bool flag = HeroController.instance.acceptingInput || HeroController.instance.hero_state != ActorStates.no_input;
@@ -233,6 +236,21 @@ public class ModCheats : MonoBehaviour
 		}
 	}
 
+	// Token: 0x1700043D RID: 1085
+	// (get) Token: 0x0600304B RID: 12363 RVA: 0x0002432D File Offset: 0x0002252D
+	// (set) Token: 0x0600304C RID: 12364 RVA: 0x00024335 File Offset: 0x00022535
+	private PlayerData pd { get; set; }
+
+	// Token: 0x1700043E RID: 1086
+	// (get) Token: 0x0600304D RID: 12365 RVA: 0x0002433E File Offset: 0x0002253E
+	// (set) Token: 0x0600304E RID: 12366 RVA: 0x00024346 File Offset: 0x00022546
+	private HeroController hc { get; set; }
+
+	// Token: 0x1700043F RID: 1087
+	// (get) Token: 0x0600304F RID: 12367 RVA: 0x0002434F File Offset: 0x0002254F
+	// (set) Token: 0x06003050 RID: 12368 RVA: 0x00024357 File Offset: 0x00022557
+	private GameManager gm { get; set; }
+
 	// Token: 0x04003809 RID: 14345
 	private bool noclip;
 
@@ -283,4 +301,13 @@ public class ModCheats : MonoBehaviour
 
 	// Token: 0x04003819 RID: 14361
 	public string SaveScene;
+
+	// Token: 0x0400381D RID: 14365
+	public int SaveMaxMP;
+
+	// Token: 0x0400381E RID: 14366
+	public int SaveShadeGeo;
+
+	// Token: 0x0400381F RID: 14367
+	public int SaveShadeMP;
 }
